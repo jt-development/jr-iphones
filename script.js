ab6a3887ef038c1ddaad7e850ec2b77d4a6157fa
jt-development <63732103+jt-development@users.noreply.github.com>
2020-12-08 20:04:48 -0300

    novas taxas

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.aviso') || []).forEach(($delete) => {
-    $notification = $delete.parentNode;
+	(document.querySelectorAll('.aviso') || []).forEach(($delete) => {
+		$notification = $delete.parentNode;
 
-    $delete.addEventListener('click', () => {
-      document.querySelectorAll('.notification')[0].remove();
-    });
-  });
+		$delete.addEventListener('click', () => {
+			document.querySelectorAll('.notification')[0].remove();
+		});
+	});
 });
 
+
 function calculaJuros() {
-	let valorFinanciado =  parseFloat(document.querySelector('#valor').value)
-	let parcelas =  parseInt(document.querySelector('#parcelas').value)
+	const taxasMaster = [3.87, 4.36, 4.85, 5.35, 5.84, 6.43, 6.92, 7.41, 7.91, 8.40, 8.89];
+	const taxaDesc = 0.8;
+	const taxasJuros = taxasMaster.map(taxa => (taxa + taxaDesc).toFixed(2)).map(Number);
 
-		if(!valorFinanciado){
-			document.querySelector('.aviso').innerHTML = `<div class="notification is-danger">
-			<button class="delete"></button> É necessario informar um valor antes de calcular</div>`
-		}else{
-			let juros = (parcelas + 2) /100
-			let valorDaParcela = ((valorFinanciado * juros) + valorFinanciado) / parcelas
-			valorDaParcela = valorDaParcela.toFixed(2)
-			document.querySelector('.aviso').innerHTML = `<div class="notification is-success">
-			<button class="delete"></button></button> ${parcelas} Vezes de R$${valorDaParcela}</div>`
-			//document.querySelector('.aviso').innerHTML = `<strong>R$ ${valorFinanciado} em ${parcelas}X fica... ${valorDaParcela}</strong>`
-		}
+	const valorFinanciado = parseFloat(document.querySelector('#valor').value);
+	const parcelas = parseInt(document.querySelector('#parcelas').value);
+
+	if (!valorFinanciado) {
+		document.querySelector('.aviso').innerHTML = `<div class="notification is-danger">
+			<button class="delete"></button> É necessário informar um valor antes de calcular</div>`;
+	} else {
+
+		let juros = taxasJuros[parcelas - 2] / 100
+		let valorDaParcela = ((valorFinanciado * juros) + valorFinanciado) / parcelas;
+		valorDaParcela = valorDaParcela.toFixed(2);
+
+		document.querySelector('.aviso').innerHTML = `<div class="notification is-success">
+			<button class="delete"></button></button> ${parcelas} Vezes de R$${valorDaParcela}</div>`;
+		document.querySelector('#valor').value = "";
+	}
+
 }
-
-//document.querySelectorAll('.notification .delete')
-//parentNode.removeChild($notification);
